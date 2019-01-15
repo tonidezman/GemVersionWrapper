400454575b2e789c1647fe60d2b2aa5ea092bece
Toni Dezman <toni.dezman@gmail.com>
2019-01-15 16:06:49 +0100

    implements the code

--- lib/gem_version_wrapper.rb
+++ lib/gem_version_wrapper.rb
@@ -1,4 +1,29 @@
 require "gem_version_wrapper/version"
 
 module GemVersionWrapper
+  def self.build(str)
+    begin
+      Gem::Version.new str
+    rescue ArgumentError => e
+      if defined? Rails.logger.error
+        Rails.logger.error("#{e}: invalid version input from DB => Gem::Version.new(#{str})")
+      end
+
+      handle_invalid str
+    end
+  end
+
+  private
+
+  def self.handle_invalid(str)
+    if unprocessable_input? str
+      Gem::Version.new ''
+    else
+      Gem::Version.new str.to_f
+    end
+  end
+
+  def self.unprocessable_input?(str)
+    str.to_f == 0.0
+  end
 end
