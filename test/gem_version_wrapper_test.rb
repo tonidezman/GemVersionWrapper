daec7f9c2751d508ef79b5e84f42ba065600280c
Toni Dezman <toni.dezman@gmail.com>
2019-01-15 16:07:03 +0100

    adds tests

--- test/gem_version_wrapper_test.rb
+++ test/gem_version_wrapper_test.rb
@@ -6,6 +6,67 @@ class GemVersionWrapperTest < Minitest::Test
   end
 
   def test_it_does_something_useful
-    assert false
+    assert true
   end
+
+  def test_input_is_string_1
+    actual_value   = GemVersionWrapper.build('1.0')
+    expected_value = Gem::Version.new('1.0')
+    assert_equal expected_value, actual_value
+  end
+
+  def test_input_is_string_2
+    actual_value   = GemVersionWrapper.build('1')
+    expected_value = Gem::Version.new('1')
+    assert_equal expected_value, actual_value
+  end
+
+  def test_input_is_nil
+    actual_value   = GemVersionWrapper.build(nil)
+    expected_value = Gem::Version.new('')
+    assert_equal expected_value, actual_value
+  end
+
+  def test_input_empty_string
+    actual_value   = GemVersionWrapper.build('')
+    expected_value = Gem::Version.new('')
+    assert_equal expected_value, actual_value
+  end
+
+  def test_input_number_with_string
+    actual_value   = GemVersionWrapper.build('foo5')
+    expected_value = Gem::Version.new('')
+    assert_equal expected_value, actual_value
+  end
+
+  def test_input_string_with_number
+    actual_value   = GemVersionWrapper.build('5foo')
+    expected_value = Gem::Version.new('5.0')
+    assert_equal expected_value, actual_value
+  end
+
+  def test_invalid_input
+    actual_value   = GemVersionWrapper.build('foo')
+    expected_value = Gem::Version.new('')
+    assert_equal expected_value, actual_value
+  end
+
+  def test_invalid_input_2
+    actual_value   = GemVersionWrapper.build('1.')
+    expected_value = Gem::Version.new('1.0')
+    assert_equal expected_value, actual_value
+  end
+
+  def test_invalid_input_3
+    actual_value   = GemVersionWrapper.build('.3')
+    expected_value = Gem::Version.new('0.3')
+    assert_equal expected_value, actual_value
+  end
+
+  def test_integer
+    actual_value   = GemVersionWrapper.build(15)
+    expected_value = Gem::Version.new('15.0')
+    assert_equal expected_value, actual_value
+  end
+
 end
